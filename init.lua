7497ff491a88c9186fa779d17aa5b7a6fc6d5c05
Mahmoud ElShaikh <moudelshaikh@outlook.com>
2025-09-19 20:10:08 +0300

    removes preselection from blink.

--- init.lua
+++ init.lua
@@ -1385,6 +1385,13 @@ require('lazy').setup({
       },
 
       completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
