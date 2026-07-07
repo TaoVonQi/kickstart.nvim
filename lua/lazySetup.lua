5bde9fd9253738e2f15e29401234f5dc1427f5e0
Mahmoud ElShaikh <moudelshaikh@outlook.com>
2026-07-07 10:14:17 +0300

    chore: configure mini.surround keybindings and update which-key groups
    - Set up mini.surround with custom leader mappings (add, delete, find, find_left, highlight, replace, update_n_lines) to avoid conflict with 's' in normal mode.

--- lua/lazySetup.lua
+++ lua/lazySetup.lua
@@ -212,7 +212,17 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = '<leader>na',
+          delete = '<leader>nd',
+          find = '<leader>nf',
+          find_left = '<leader>nF',
+          highlight = '<leader>nh',
+          replace = '<leader>nr',
+          update_n_lines = '<leader>nn',
+        },
+      }
     end,
   },
 
